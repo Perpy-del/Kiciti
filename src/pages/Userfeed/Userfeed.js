8f3630181dbcb14c1f63375e3ac115dda7ed2c99
Perpy-del <perpetualmeninwa@gmail.com>
2023-01-06 19:56:40 +0100

    Made new changes

--- src/pages/Userfeed/Userfeed.js
+++ src/pages/Userfeed/Userfeed.js
@@ -14,6 +14,7 @@ import ellipsis from './images/mdi_ellipsis.svg';
 import notification from './images/notifications.svg';
 import logout from './images/logout.svg';
 import userprofile from './images/user_profile.png';
+import home from './images/home.svg';
 
 const Userfeed = () => {
   // const [ profile, setProfile ] = useState({ preview: "", data: "" });
@@ -29,28 +30,27 @@ const Userfeed = () => {
   return (
     <>
       <div className='feed-header'>
-        <Link><img src={help} alt='help-icon' /></Link>
+        <Link><img src={help} alt='help-icon' id='help'/></Link>
         <h3> Feed </h3>
         <div className='icon-set'>
           <div>
-            <Link to='/'><img src={notification} alt='notification-icon' /></Link>
+            <Link to='/'><img src={home} alt='home-icon' id='i-set' /></Link>
           </div>
-          <div>
-            <Link><img src={notification} alt='notification-icon' /></Link>
+          <div id='i-set'>
+            <Link><img src={notification} alt='notification-icon' id='i-set' /></Link>
           </div>
-          <div>
-            <Link to='/profile'><img src={userprofile} alt='user-profile-icon' /></Link>
+          <div id='i-set'>
+            <Link to='/profile'><img src={userprofile} alt='user-profile-icon' id='i-set' /></Link>
           </div>
           <div className='search'>
             <img src={search} alt='search-icon' />
             <input type='text' placeholder='Search'/>  
           </div>
         </div>
-        <div className='lg-icon'>
-          <Link to='/login' id='logout'>
-            <img src={logout} alt='logout-icon' />
+        <div>
+          <Link to='/login' id='logout' >
+            <img src={logout} alt='logout-icon' id='i-set' />
           </Link>
-          <h6 className='hide'>Logout</h6>
         </div>
       </div>
 
@@ -63,45 +63,69 @@ const Userfeed = () => {
         </div>
 
         <div className='post'>
-          <img src={profileimage} alt='profile-icon' />
+          <div className='primary_post'>
+            <img src={profileimage} alt='profile-icon' id='profile'/>
+            <div className='post_content'>
+              <h4>Van Nguyen</h4>
+              <h6>Today at 3:08 AM</h6>
+              <h6 id='comments'>My baby will be here in two weeks</h6>
+            </div>
+          </div>  
           <div>
-            <h4>Van Nguyen</h4>
-            <h6>Today at 3:08 AM</h6>
-            <h6 id='comments'>My baby will be here in two weeks</h6>
+            <img src={ellipsis} alt="ellipsis-icon" id='ellipsis'/>
           </div>
-          <img src={ellipsis} alt="ellipsis-icon" id='ellipsis'/>
+          
         </div>
         <div className='social-icon'>
           <div>
-            <img src={fav} alt="favorite-icon" />
+            <button>
+              <img src={fav} alt="favorite-icon" /> 
+            </button>
             <h6>1,223 Likes</h6>
           </div>
           <div>
-            <img src={comment} alt="comment" />
+            <button>
+              <img src={comment} alt="comment" />
+            </button>
             <h6>560 Comments</h6>
           </div>
         </div>
 
-        <div className='post'>
-          <img src={profile} alt='profile-icon' id='profile' />
-          <div>
-            <h4>Me Bhan Ho</h4>
-            <h6>Just Now</h6>
-            <h6 id='comments'>He is here and he is perfect</h6>
-          </div>
-          <img src={ellipsis} alt="ellipsis-icon" id='ellipsis2'/>
+        <div>
+          <input type="text" placeholder="Add a Comment" id='add_comment' />
         </div>
-        <img src={baby} alt="baby" id='babypic' />
+
+        <div className='post'>
+          <div className='primary_post'>
+            <img src={profile} alt='profile-icon' id='profile' />
+            <div className='post_content'>
+              <h4>Me Bhan Ho</h4>
+              <h6>Just Now</h6>
+              <h6 id='comments'>He is here and he is perfect</h6>
+            </div>
+          </div>
+          <div>
+            <button>
+            <img src={ellipsis} alt="ellipsis-icon" id='ellipsis'/>
+            </button>
+          </div>
+        </div>
         <div className='social-icon'>
           <div>
+            <button>
             <img src={fav} alt="favorite-icon" />
+            </button>
             <h6>1,223 Likes</h6>
           </div>
           <div>
+            <button>
             <img src={comment} alt="comment" />
+            </button>
             <h6>560 Comments</h6>
           </div>
         </div>
+        <img src={baby} alt="baby" id='babypic' />
+        
       </div>
     </>
   )

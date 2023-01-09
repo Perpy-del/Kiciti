67b4519919c85b06ee9cfe51170b3fcd567e9a69
Teepee78 <eniolatoluwalase78@gmail.com>
2023-01-09 15:20:42 +0100

    Handle pictures in profile page

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -5,6 +5,8 @@ import chevron from "./images/arrow_back.svg";
 import profileimage from "./images/profile_img.svg";
 import ellipsis from "./images/mdi_ellipsis.svg";
 import { useNavigate } from "react-router-dom";
+import fav from "../Posts/images/favorite-icon.svg";
+import comment from "../Posts/images/comments-icon.svg";
 
 const Profile = () => {
   const [firstName, setFirstName] = useState("");
@@ -17,6 +19,7 @@ const Profile = () => {
   const [country, setCountry] = useState("");
   const [error, setError] = useState(false);
   const [pfp, setPfp] = useState("");
+  const [posts, setPosts] = useState([]);
 
   const user_token = localStorage.getItem("X-auth-token");
   const user_id = localStorage.getItem("user_id");
@@ -67,9 +70,79 @@ const Profile = () => {
     }
   };
 
+  // Fetch posts from database
+  const fetchPosts = async () => {
+    try {
+      const response = await fetch(`http://34.228.198.103/api/posts/${user_id}`, {
+        method: "GET",
+        headers: {
+          "X-auth-token": user_token,
+        },
+      });
+      const data = await response.json();
+      const posts = [];
+
+      for (let post of data.posts) {
+        if (post === null) {
+          continue;
+        }
+
+        posts.push(
+          <div className="main-posts" key={post._id}>
+            <div className="post">
+              <div className="primary_post">
+                <img src={pfp} alt="profile-icon" id="profile" />
+                <div className="post_content">
+                  <h4>
+                    {firstName.charAt(0).toUpperCase() +
+                      firstName.slice(1)}
+                    {middleName !== null
+                      ? " " +
+                        middleName.charAt(0).toUpperCase() +
+                        middleName.slice(1)
+                      : ""}{" "}
+                    {lastName.charAt(0).toUpperCase() +
+                      lastName.slice(1)}
+                  </h4>
+                  <h6>{new Date(post.created_at).toDateString()}</h6>
+                  <h6 id="comments">{post.content}</h6>
+                </div>
+              </div>
+              <div>
+                <button>
+                  <img src={ellipsis} alt="ellipsis-icon" id="ellipsis" />
+                </button>
+              </div>
+            </div>
+            {/* Render post image*/}
+            <div className="social-icon">
+              <div>
+                <button>
+                  <img src={fav} alt="favorite-icon" />
+                </button>
+                <h6>{post.likes.length}</h6>
+              </div>
+              <div>
+                <button>
+                  <img src={comment} alt="comment" />
+                </button>
+                <h6>{post.comments.length}</h6>
+              </div>
+            </div>
+          </div>
+        );
+        setPosts(posts);
+      }
+    } catch (error) {
+      console.error(error);
+      navigate("/login");
+    }
+  };
+
   useEffect(() => {
     fetchUser();
     fetchPfp();
+    fetchPosts();
   }, []);
 
   return (
@@ -94,7 +167,9 @@ const Profile = () => {
         <div className="mid-content">
           <h1>
             {firstName.charAt(0).toUpperCase() + firstName.slice(1)}
-            {middleName !== null ? " " + middleName.charAt(0).toUpperCase() + middleName.slice(1) : ""}{" "}
+            {middleName !== null
+              ? " " + middleName.charAt(0).toUpperCase() + middleName.slice(1)
+              : ""}{" "}
             {lastName.charAt(0).toUpperCase() + lastName.slice(1)}
           </h1>
           <h3>About</h3>
@@ -112,11 +187,11 @@ const Profile = () => {
           <div>
             <button id="posts">Posts</button>
             <button id="likes">Likes</button>
-            <button id="rooms">Rooms</button>
+            {/* <button id="rooms">Rooms</button> */}
           </div>
         </div>
 
-        <div className="post_view">
+        {/* <div className="post_view">
           <img src={profileimage} alt="profile_image" />
           <div>
             <h5>Emily Vonne</h5>
@@ -137,7 +212,9 @@ const Profile = () => {
           <button id="more">
             <img src={ellipsis} alt="ellipsis" />
           </button>
-        </div>
+        </div> */}
+
+        {posts}
       </div>
     </>
   );
